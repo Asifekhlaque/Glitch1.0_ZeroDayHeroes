a627f0209e8c13a011806347e19dc5ba9ea74ec8
Asif Ekhlaque <asifekhlaque33@gmail.com>
2025-09-12 17:07:08 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,6 +2,7 @@ import AppSidebar from '@/components/app-sidebar';
 import AppHeader from '@/components/app-header';
 import AppBottomNav from '@/components/app-bottom-nav';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
+import ClientOnly from '@/components/client-only';
 
 export default function AppLayout({
   children,
@@ -10,14 +11,18 @@ export default function AppLayout({
 }) {
   return (
     <SidebarProvider>
+      <ClientOnly>
         <AppHeader />
-        <AppSidebar />
-        <SidebarInset>
-          <div className="min-h-screen lg:py-8 lg:px-16 px-4 py-6 pt-20 lg:pt-8 pb-24 lg:pb-8">
-            {children}
-          </div>
-        </SidebarInset>
+      </ClientOnly>
+      <AppSidebar />
+      <SidebarInset>
+        <div className="min-h-screen lg:py-8 lg:px-16 px-4 py-6 pt-20 lg:pt-8 pb-24 lg:pb-8">
+          {children}
+        </div>
+      </SidebarInset>
+      <ClientOnly>
         <AppBottomNav />
+      </ClientOnly>
     </SidebarProvider>
   );
 }
