7b27e0e7258fab5a101e671ee705d6ad17f4176c
Vishuu Raj <lt.vishalkumar8067@gmail.com>
2025-09-19 06:46:21 +0000

    Try fixing this error: `Runtime Error: Error: ./src/components/app-heade

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -26,20 +26,13 @@ export function ThemeToggle() {
 
   return (
     <DropdownMenu>
-      <Tooltip>
-        <TooltipTrigger asChild>
-            <DropdownMenuTrigger asChild>
-                <SidebarMenuButton variant="ghost" className="w-full justify-start">
-                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span className="group-data-[collapsible=icon]:hidden">Toggle Theme</span>
-                </SidebarMenuButton>
-            </DropdownMenuTrigger>
-        </TooltipTrigger>
-        <TooltipContent>
-            <p>Toggle Theme</p>
-        </TooltipContent>
-      </Tooltip>
+      <DropdownMenuTrigger asChild>
+        <SidebarMenuButton variant="ghost" className="w-full justify-start" tooltip="Toggle Theme">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="group-data-[collapsible=icon]:hidden">Toggle Theme</span>
+        </SidebarMenuButton>
+      </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
